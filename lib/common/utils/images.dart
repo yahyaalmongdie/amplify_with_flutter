8eab7e298ce3fb830b1a118ba5329a5df87fa04a
yahya almongdie <yahyaalmongdie@gmail.com>
2024-12-01 21:42:13 +0300

    create app images class

---
+++ lib/common/utils/images.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imageImagesAmplify
+  /// assets/images/amplify.png
+  static const String imageImagesAmplify = "assets/images/amplify.png";
+}
+
