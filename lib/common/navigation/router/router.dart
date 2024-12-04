59d4fe7943e642e7d3731afcd5044942d2a7425c
yahya almongdie <yahyaalmongdie@gmail.com>
2024-12-04 21:55:31 +0300

    add trip page to route

--- lib/common/navigation/router/router.dart
+++ lib/common/navigation/router/router.dart
@@ -1,5 +1,6 @@
 import 'package:amplify_with_flutter/common/navigation/router/routes.dart';
 import 'package:amplify_with_flutter/features/trip/ui/trip_list/trip_list_page.dart';
+import 'package:amplify_with_flutter/features/trip/ui/trip_page/trip_page.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -9,6 +10,14 @@ final router = GoRouter(
       path: '/',
       name: AppRoute.home.name,
       builder: (context, state) => const TripsListPage(),
+    ),
+        GoRoute(
+      path: '/trip/:id',
+      name: AppRoute.trip.name,
+      builder: (context, state) {
+        final tripId = state.pathParameters['id']!;
+        return TripPage(tripId: tripId);
+      },
     ),
   ],
   errorBuilder: (context, state) => Scaffold(
