bcf7df440abdb53f1091224866ad6b9799549565
yahya almongdie <yahyaalmongdie@gmail.com>
2024-12-04 21:28:34 +0300

    create trip controller

--- lib/common/ui/upload_progress_dialog.dart
+++ lib/common/ui/upload_progress_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:amplify_with_flutter/features/trip/controller/trip_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
