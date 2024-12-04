8d128c76a414124064e84d4b5ba02bde3d24cfa7
yahya almongdie <yahyaalmongdie@gmail.com>
2024-12-04 21:59:00 +0300

    add edit  trip page to route

--- lib/common/navigation/router/router.dart
+++ lib/common/navigation/router/router.dart
@@ -1,6 +1,8 @@
 import 'package:amplify_with_flutter/common/navigation/router/routes.dart';
+import 'package:amplify_with_flutter/features/trip/ui/edit_trip_page/edit_trip_page.dart';
 import 'package:amplify_with_flutter/features/trip/ui/trip_list/trip_list_page.dart';
 import 'package:amplify_with_flutter/features/trip/ui/trip_page/trip_page.dart';
+import 'package:amplify_with_flutter/models/Trip.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -19,6 +21,15 @@ final router = GoRouter(
         return TripPage(tripId: tripId);
       },
     ),
+        GoRoute(
+      path: '/edittrip/:id',
+      name: AppRoute.editTrip.name,
+      builder: (context, state) {
+        return EditTripPage(
+          trip: state.extra! as Trip,
+        );
+      },
+    ),  
   ],
   errorBuilder: (context, state) => Scaffold(
     body: Center(
