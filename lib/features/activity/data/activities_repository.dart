35e9fb05290f82694bcbdf499bb3e9e784f0d3b7
yahya almongdie <yahyaalmongdie@gmail.com>
2024-12-06 20:15:02 +0300

    create activities repository

---
+++ lib/features/activity/data/activities_repository.dart
@@ -0,0 +1,37 @@
+
+import 'package:amplify_with_flutter/features/activity/service/activities_api_service.dart';
+import 'package:amplify_with_flutter/models/Activity.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final activitiesRepositoryProvider = Provider<ActivitiesRepository>((ref) {
+  final activitiesAPIService = ref.read(activitiesAPIServiceProvider);
+  return ActivitiesRepository(activitiesAPIService);
+});
+
+class ActivitiesRepository {
+  ActivitiesRepository(
+    this.activitiesAPIService,
+  );
+
+  final ActivitiesAPIService activitiesAPIService;
+
+  Future<List<Activity>> getActivitiesForTrip(String tripId) {
+    return activitiesAPIService.getActivitiesForTrip(tripId);
+  }
+
+  Future<Activity> getActivity(String activityId) {
+    return activitiesAPIService.getActivity(activityId);
+  }
+
+  Future<void> add(Activity activity) async {
+    return activitiesAPIService.addActivity(activity);
+  }
+
+  Future<void> delete(Activity activity) async {
+    return activitiesAPIService.deleteActivity(activity);
+  }
+
+  Future<void> update(Activity activity) async {
+    return activitiesAPIService.updateActivity(activity);
+  }
+}
