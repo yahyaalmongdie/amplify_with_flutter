83669a591b07b3030955bd7ad0f1c53e90cb985f
yahya almongdie <yahyaalmongdie@gmail.com>
2024-12-06 20:03:04 +0300

    create  past trip page

---
+++ lib/features/trip/ui/past_trip_page/past_trip_page.dart
@@ -0,0 +1,48 @@
+
+import 'package:amplify_with_flutter/common/navigation/router/routes.dart';
+import 'package:amplify_with_flutter/common/ui/the_navigation_drawer.dart';
+import 'package:amplify_with_flutter/common/utils/colors.dart';
+import 'package:amplify_with_flutter/features/trip/controller/trip_controller.dart';
+import 'package:amplify_with_flutter/features/trip/ui/past_trip_page/past_trip_details.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class PastTripPage extends ConsumerWidget {
+  const PastTripPage({
+    required this.tripId,
+    super.key,
+  });
+  final String tripId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tripValue = ref.watch(tripControllerProvider(tripId));
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Amplify Trips Planner',
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.goNamed(
+                AppRoute.home.name,
+              );
+            },
+            icon: const Icon(Icons.home),
+          ),
+        ],
+        backgroundColor:  Color(primaryColorDark),
+      ),
+      drawer: const TheNavigationDrawer(),
+      body: ColorFiltered(
+        colorFilter: const ColorFilter.matrix(greyoutMatrix),
+        child: PastTripDetails(
+          trip: tripValue,
+        ),
+      ),
+    );
+  }
+}
