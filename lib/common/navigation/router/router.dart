a8181edd8fcc43a039320f3cb1924e66c523a99c
yahya almongdie <yahyaalmongdie@gmail.com>
2024-12-06 20:03:41 +0300

    add  past trip pageto route

--- lib/common/navigation/router/router.dart
+++ lib/common/navigation/router/router.dart
@@ -1,5 +1,6 @@
 import 'package:amplify_with_flutter/common/navigation/router/routes.dart';
 import 'package:amplify_with_flutter/features/trip/ui/edit_trip_page/edit_trip_page.dart';
+import 'package:amplify_with_flutter/features/trip/ui/past_trip_page/past_trip_page.dart';
 import 'package:amplify_with_flutter/features/trip/ui/pats_trip/past_trips_list.dart';
 import 'package:amplify_with_flutter/features/trip/ui/trip_page/trip_page.dart';
 import 'package:amplify_with_flutter/features/trip/ui/trip_list/trips_list_page.dart';
@@ -35,6 +36,14 @@ final router = GoRouter(
       path: '/pasttrips',
       name: AppRoute.pastTrips.name,
       builder: (context, state) => const PastTripsList(),
+    ),
+        GoRoute(
+      path: '/pasttrip/:id',
+      name: AppRoute.pastTrip.name,
+      builder: (context, state) {
+        final tripId = state.pathParameters['id']!;
+        return PastTripPage(tripId: tripId);
+      },
     ),
   ],
   errorBuilder: (context, state) => Scaffold(
